82fd6b2b596876bdc2ec2719ef45cc890a8399d4
Victoria Fields <victoriaashleyfields@gmail.com>
2022-10-13 07:50:09 -0600

    Create Form component skeleton, connect to App

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Form from '../Form/Form.js';
 
 class App extends React.Component {
   constructor() {
@@ -13,6 +14,7 @@ class App extends React.Component {
     return (
       <main>
         <h1>My To Dos</h1>
+        <Form />
       </main>
     )
   }
---
+++ src/Form/Form.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Form.css';
+
+class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      description: ''
+    }
+  }
+
+  render() {
+    return (
+      <p>Check</p>
+    )
+  }
+}
+
+export default Form
